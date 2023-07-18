git-roguexbyu9/bitbox-wallet-app

commit 87b820d99dedf36050e89d7740987b03f405e2b7

frontend: fix layout of manage accounts with long account names
On small screen long accountnames/edit/toggle break in various
different ways.

Fixed by letting the name break, so that edit and toggle are always
on the same line.

frontends/web/src/routes/settings/manage-accounts.module.css
@@ -7,7 +7,7 @@
     align-items: center;
     display: flex;
     flex-direction: row;
-    flex-wrap: wrap;
+    flex-wrap: nowrap;
     justify-content: flex-end;
     min-height: var(--item-height);
     padding: 10px var(--space-half);
@@ -28,6 +28,7 @@
     background: transparent;
     border: none;
     color: var(--color-primary);
+    flex-shrink: 0;
     line-height: 2;
     margin-right: var(--spacing-default);
 }

commit 1ed9ecfc8b5418eda7f5e6c7d0908656f30231cd

frontend: fix css regression in manage accounts
Tokens did not break correctly to their own section.

Regression introduced in 87b820d99dedf36050e89d7740987b03f405e2b7

This is only visible with make servewallet-mainnet as tokens are
only shown in mainnet.

frontends/web/src/routes/settings/manage-accounts.module.css
@@ -7,7 +7,7 @@
     align-items: center;
     display: flex;
     flex-direction: row;
-    flex-wrap: nowrap;
+    flex-wrap: wrap;
     justify-content: flex-end;
     min-height: var(--item-height);
     padding: 10px var(--space-half);
@@ -21,16 +21,26 @@
 .acccountLink {
     align-items: center;
     display: flex;
-    flex-grow: 1;
+    flex-basis: calc(100% - 180px);
+    flex-grow: 0;
+    flex-shrink: 1;
 }
 
 .editBtn {
     background: transparent;
     border: none;
     color: var(--color-primary);
-    flex-shrink: 0;
+    flex-basis: content;
+    flex-grow: 1;
+    flex-shrink: 1;
     line-height: 2;
-    margin-right: var(--spacing-default);
+    text-align: right;
+}
+
+.toggle {
+    flex-basis: 60px;
+    flex-shrink: 0;
+    margin-left: var(--spacing-half);
 }
 
 .accountActive {

frontends/web/src/routes/settings/manage-accounts.tsx
@@ -93,6 +93,7 @@ class ManageAccounts extends Component<Props, State> {
           </button>
           <Toggle
             checked={active}
+            className={style.toggle}
             id={account.code}
             onChange={(event) => {
               event.target.disabled = true;
@@ -173,6 +174,7 @@ class ManageAccounts extends Component<Props, State> {
           </div>
           <Toggle
             checked={active}
+            className={style.toggle}
             id={token.code}
             onChange={() => this.toggleToken(ethAccountCode, token.code, !active)} />
         </div>