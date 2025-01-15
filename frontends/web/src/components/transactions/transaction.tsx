git-roguexbyu9/bitbox-wallet-app

commit 090038f3baff300a37b621c6a12617890709ca96

frontend: hide estimate sign if conversion is btc or sat
Until a transaction has 6 confirmations we do show a estimation sign
for the conversion amount as the exact time is still not final.

The user can optionally choose BTC or sat as conversion unit in
these cases no estimation sign is needed.

frontends/web/src/components/transactions/transaction.tsx
@@ -167,6 +167,7 @@ const Amounts = ({
   const conversionPrefix = amount.estimated ? '\u2248' : null; // ≈
   const sendToSelf = type === 'send_to_self';
   const conversionUnit = sendToSelf ? amount.unit : defaultCurrency;
+  const conversionIsFiat = conversionUnit !== 'BTC' && conversionUnit !== 'sat';
 
   return (
     <span className={`${styles.txAmountsColumn} ${styles[txTypeClass]}`}>
@@ -189,7 +190,7 @@ const Amounts = ({
             <Arrow type="send_to_self" />
           </span>
         )}
-        {(conversionPrefix && !sendToSelf) && (
+        {(conversionPrefix && !sendToSelf && conversionIsFiat) && (
           <span className={styles.txPrefix}>
             {conversionPrefix}
             {' '}

commit 7507cc4fc4a2bcc6ab2f3609ba70951099a61913

frontend: show estimate symbol for pending alt coin transactions
The app should show an estimate symbol for unconfirmed transactions
unless the conversion unit and coin unit are both BTC based.

With last commit the logic only checked for conversion unit but
ignored the actual coin unit, so alt coins didn't have an estimate
symbol even though their BTC/sat amount was not final or still
and estimate.

Regression introduced in 090038f3baff300a37b621c6a12617890709ca96

frontends/web/src/components/transactions/transaction.tsx
@@ -168,7 +168,10 @@ const Amounts = ({
   const conversionPrefix = amount.estimated ? '\u2248' : null; // ≈
   const sendToSelf = type === 'send_to_self';
   const conversionUnit = sendToSelf ? amount.unit : defaultCurrency;
+  const currencyNotBitcoin = defaultCurrency !== 'BTC' && defaultCurrency !== 'sat';
   const conversionIsFiat = conversionUnit !== 'BTC' && conversionUnit !== 'sat';
+
+  const withConversionSymbol = currencyNotBitcoin && conversionIsFiat && !sendToSelf && conversionPrefix;
 
   return (
     <span className={`${styles.txAmountsColumn} ${styles[txTypeClass]}`}>
@@ -191,7 +194,7 @@ const Amounts = ({
             <Arrow type="send_to_self" />
           </span>
         )}
-        {(conversionPrefix && !sendToSelf && conversionIsFiat) && (
+        {withConversionSymbol && (
           <span className={styles.txPrefix}>
             {conversionPrefix}
             {' '}